5a2cd349bb6f7faa78dd4033ca9074be32bdcc6d
Sneha Parande <99521196+snehaparande@users.noreply.github.com>
2022-06-03 18:58:50 +0530

    Started 'animalInstincts'

---
+++ animalInstincts.js
@@ -0,0 +1,29 @@
+const { EventEmitter } = require('events');
+
+const moveFront = () => console.log('Move Front');
+const turnLeft = () => console.log('Turn Left >');
+const turnRight = () => console.log('< Turn Right');
+
+const randomInt = (limit) => Math.floor(Math.random() * limit);
+
+const startGame = (events, eventEmitter) => {
+  setInterval(() => {
+    const event = events[randomInt(events.length)];
+    eventEmitter.emit(event);
+  }, 1000);
+};
+
+const main = () => {
+
+  const eventEmitter = new EventEmitter();
+
+  eventEmitter.on('maw-maw', moveFront);
+  eventEmitter.on('bhow-bhow', turnLeft);
+  eventEmitter.on('kaa-kaa', turnRight);
+
+  const events = eventEmitter.eventNames();
+
+  startGame(events, eventEmitter);
+};
+
+main();
