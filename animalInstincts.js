15614b8010483ce13f402809d0323ba0b1d66c9b
Sneha Parande <99521196+snehaparande@users.noreply.github.com>
2022-06-06 12:07:26 +0530

    Added target position for person.
    - Added events for moving back and reaching the target

--- animalInstincts.js
+++ animalInstincts.js
@@ -5,30 +5,51 @@ const { Person } = require('./person.js');
 const latestEvent = (instructions) =>
   instructions.trim().split('\n').slice(-1)[0];
 
-const startGame = (eventEmitter) => {
+const startGame = (eventEmitter, person) => {
 
   fs.watchFile('instructions.txt', () => {
 
     const events = fs.readFileSync('instructions.txt', 'utf8');
     const event = latestEvent(events);
     eventEmitter.emit(event);
+    if (person.reached()) {
+      eventEmitter.emit('reached-target');
+      fs.unwatchFile('instructions.txt');
+    }
   });
 
 };
 
-const main = () => {
+const randomInt = (limit) => Math.floor(Math.random() * limit);
 
-  const eventEmitter = new EventEmitter();
-  const person = new Person;
+const createTarget = () => {
+  return { x: randomInt(10), y: randomInt(10) };
+};
 
+const winningMessage = () =>
+  console.log('Congrats!!!\n You reached the target.');
+
+const addEvents = (eventEmitter, person) => {
   eventEmitter.on('maw-maw', () => person.moveFront());
   eventEmitter.on('maw-maw', () => person.toString());
   eventEmitter.on('bhow-bhow', () => person.moveLeft());
   eventEmitter.on('bhow-bhow', () => person.toString());
   eventEmitter.on('kaa-kaa', () => person.moveRight());
   eventEmitter.on('kaa-kaa', () => person.toString());
+  eventEmitter.on('mhehe', () => person.moveBack());
+  eventEmitter.on('mhehe', () => person.toString());
+  eventEmitter.on('reached-target', winningMessage);
+};
 
-  startGame(eventEmitter);
+const main = () => {
+
+  const eventEmitter = new EventEmitter();
+  const target = createTarget();
+  const person = new Person(target);
+
+  addEvents(eventEmitter, person);
+
+  startGame(eventEmitter, person);
 };
 
 main();
